b9fa5102d418bd88fed038d1a527f1ad3c9372fd
Nop3e3 <salma.q.qorashy@gmail.com>
2026-04-15 11:16:19 +0200

    signin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Signup from "./Pages/Signup";
-
+import Signin from "./Pages/Signin"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Signup/>} />
-   
+      <Route path="/Signin" element={<Signin/>} />
     </Routes>
   );
 }
